3cd858a50323ae7b3f68613a026559c0b27cbceb
Samarth1911 <78640190+Samarth1911@users.noreply.github.com>
2021-05-11 20:17:29 +0530

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,19 @@
+class Hero{
+    constructor(x,y,radius){
+        var options = {
+            isStatc : false
+        }
+        this.body = Bodies.circle(x,y,radius,options)
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.image = loadImage("")
+        World.add(world,this.body)
+
+    }
+    display(){
+        var pos = this.body.position
+        imageMode(CENTER)
+        image(this.image,pos.x,pos.y,this.radius)
+    }
+}
